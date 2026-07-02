82e95cfa8bece99a1fad01a2e3cc4fe987837949
Hearmon Tesfay Gebreyohanes <hearmon.ugr-4747-16@aau.edu.et>
2026-07-02 16:16:57 +0300

    Add files via upload
    added config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+import pygame
+
+Row = 20
+Column = 20
+Cell_length = 30
+Width = Column * Cell_length
+Height = Row * Cell_length
+Speed = 60
+
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
